6cf53b177c8247f0a83b15e95a2f7be472e1e5b9
Naoki Soga <n.soga@sunplacom>
2021-12-09 20:02:52 +0900

    sss

--- resources/views/components/post/add-post-form-genre.blade.php
+++ resources/views/components/post/add-post-form-genre.blade.php
@@ -1,4 +1,4 @@
-<select class="mt-2 py-2 px-2 rounded-md shadow border-gray-300 w-full select_{{ $genre }}" name="{{ $name }}" @if($name==="where[term]") required="true" @endif>
+<select class="mt-2 py-2 px-2 rounded-md shadow border-gray-300 w-full " name="{{ $name }}" @if($name==="where[term]") required="true" @endif>
     @if($name==="term")
         @foreach (config('postTerms.parentTerms')  as $key => $value)
             <option value="{{ $key }}" @if(isset($genre) && $genre==$key) selected @elseif($datas==$key) selected @endif>{{ $value }}</option>
