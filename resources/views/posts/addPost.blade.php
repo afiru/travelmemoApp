10e4e329c50dce13a0493d46e12e77345dbfee1b
Naoki Soga <n.soga@sunplacom>
2021-12-09 19:24:19 +0900

    sss

--- resources/views/posts/addPost.blade.php
+++ resources/views/posts/addPost.blade.php
@@ -1,13 +1,3 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ Auth::user()->name }}さん　行きたいところを入れてください
-        </h2>
-    </x-slot>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-
-        </div>
-    </div>
 </x-app-layout>
