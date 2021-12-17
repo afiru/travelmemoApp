df16d0546887448f2b610d61e8112daaabe19917
Naoki Soga <n.soga@sunplacom>
2021-12-17 18:37:31 +0900

    ss

---
+++ .history/resources/views/components/topics/topics-archives.blade_20211217183504.php
@@ -0,0 +1,25 @@
+<article class="topicsArchives">
+    @if(empty($topics ))
+        @foreach($topics as $topic)
+        <section class="secTopicsArchives">
+            <a class="buttonTopicsArchives" href="/topic/{{ $topic->id }}">
+                <time class="timeTopics">{{ $topic->UPDATED_AT }}</time>
+                <h2 class="h2TopicsArchives">{{ Str::limit($topic->title, 107, '...') }}</h2>
+            </a>
+        </section>
+        @endforeach
+    @endif
+
+
+    @if($haslink==="true")
+        <div class="buttonMoreTopicsWap">
+            <a href="/topics" class="buttonLogin buttonMoreTopics">もっと見る</a>
+        </div>
+    @endif
+
+    @if($haspaginate === "true")
+        <div class="display_flex_center paginate">
+            {{ $topics }}
+        </div>
+    @endif
+</article>
