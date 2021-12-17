fa937b37f37725fea2983a2c75a109866333532e
Naoki Soga <n.soga@sunplacom>
2021-12-17 17:46:42 +0900

    ---

--- resources/views/components/post/add-posts-search.blade.php
+++ resources/views/components/post/add-posts-search.blade.php
@@ -1,6 +1,6 @@
 <div class="searchPosts bgSearchPosts jsSearchPosts">
-    <div class="wapper serchPostsFormWap">
-        <div class="serchPostsForm">
+    <div class="wapper poRe serchPostsFormWap">
+        <div class="loginFormInput serchPostsForm">
             <form action="{{ route('posts') }}" method="get">
                 @csrf
                 <div class="formSearchPosts">
@@ -11,16 +11,16 @@
                         <x-post.add-post-form-genre name="where[subterm]" datas="" genre="{{ $where['subterm'] }}"></x-post.add-post-form-genre>
                     </div>
                     <div class="itemSearchPosts">
-                        <input class="mt-2 py-2 px-2 rounded-md shadow border-gray-300  w-full"  type="date" name="where[travelDate]" placeholder="" list="date-list" min="2022-02-26" max="2022-02-27" @if(isset($where['travelDate'])) value="{{ $where['travelDate'] }}" @endif>
+                        <input class="mt-2 py-2 px-2 rounded-md shadow itemSearchPostsInput"  type="date" name="where[travelDate]" placeholder="" list="date-list" min="2022-02-26" max="2022-02-27" @if(isset($where['travelDate'])) value="{{ $where['travelDate'] }}" @endif>
                     </div>
                     <div class="itemSearchPosts">
-                        <input class="mt-2 py-2 px-2 rounded-md shadow border-gray-300  w-full" type="text" name="where[prefectures]" placeholder="県名を入れてー" @if(isset($where['prefectures'])) value="{{ $where['prefectures'] }}" @endif>
+                        <input class="mt-2 py-2 px-2 rounded-md shadow itemSearchPostsInput" type="text" name="where[prefectures]" placeholder="県名を入れてー" @if(isset($where['prefectures'])) value="{{ $where['prefectures'] }}" @endif>
                     </div>
                     <div class="itemSearchPosts">
-                        <input class="mt-2 py-2 px-2 rounded-md shadow border-gray-300  w-full" type="text" name="where[interchange]" placeholder="インターチェンジ名" @if(isset($where['interchange'])) value="{{ $where['interchange'] }}" @endif>
+                        <input class="mt-2 py-2 px-2 rounded-md shadow itemSearchPostsInput" type="text" name="where[interchange]" placeholder="インターチェンジ名" @if(isset($where['interchange'])) value="{{ $where['interchange'] }}" @endif>
                     </div>
                     <div class="itemSearchPosts">
-                        <input class="mt-2 py-2 px-2 rounded-md shadow border-gray-300  w-full" type="text" name="where[placeName]" placeholder="場所名を入れてー"  @if(isset($where['placeName'])) value="{{ $where['placeName'] }}" @endif>
+                        <input class="mt-2 py-2 px-2 rounded-md shadow itemSearchPostsInput" type="text" name="where[placeName]" placeholder="場所名を入れてー"  @if(isset($where['placeName'])) value="{{ $where['placeName'] }}" @endif>
                     </div>
                 </div>
                 <div class="buttonSearchPostsWap">
@@ -28,7 +28,12 @@
                 </div>
             </form>
         </div>
+        <figure class="searchPostsOpen jsSearchPostsClose">
+            <img loading="lazy" src="img/buttonSearchPostsClose.svg" alt="検索を閉じる" width="80" height="50" />
+        </figure>
     </div>
+
+
 </div>
 
 
