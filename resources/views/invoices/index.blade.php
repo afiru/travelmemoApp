2e937c6b764e5ffa3ca38e7bf34076574fb3f544
Yuji Morimatsu <morimatsu-k@sunpla.com>
2021-12-21 17:46:08 +0900

    請求書ページ完了しました。

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
     <x-gnavi></x-gnavi>
     <div class="wapper loginFormWap PostsFormWaps myPageMainWap">
-        <div class="poRe border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md loginFormCnt myPageMain">
+        <div class="poRe border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md loginFormCnt myPageMain myPageMainInvoices">
             <section class="secMypage">
                 <h2 class="t_center h2Mypage">請求書</h2>
             </section>
 
-            <form action="/invoices" method="post">
+            <form class="invoicesForm" action="/invoices" method="post">
                 @csrf
                 @if(empty($invoices))
                 @else
