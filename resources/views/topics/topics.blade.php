b8d650c35e4ff44c47c29391ca1624aba52a0d4c
Yuji Morimatsu <morimatsu-k@sunpla.com>
2021-12-21 16:51:40 +0900

    --

--- resources/views/topics/topics.blade.php
+++ resources/views/topics/topics.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-gnavi></x-gnavi>
     <div class="wapper loginFormWap PostsFormWaps myPageMainWap">
-        <main class="poRe border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md loginFormCnt myPageMain">
+        <main class="poRe border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md loginFormCnt myPageMain topicsMyPageMain">
             <section class="secMypage">
                 <h2 class="t_center h2Mypage">トピックス</h2>
                 <x-topics.topics-archives limit="10" haslink=false haspaginate=true></x-topics.topics-archives>
