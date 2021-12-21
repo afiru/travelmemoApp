4adb70fb5c06af8e6b4757c265939d53874bedb0
Yuji Morimatsu <morimatsu-k@sunpla.com>
2021-12-21 16:14:27 +0900

    --

--- resources/views/topics/editotopic.blade.php
+++ resources/views/topics/editotopic.blade.php
@@ -4,7 +4,7 @@
         <main class="poRe border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md loginFormCnt myPageMain">
             <section class="secMypage">
                 <h2 class="t_center h2Mypage">トピックス編集</h2>
-                <form action="/addtopic" method="post" enctype="multipart/form-data">
+                <form class="topicsForm" action="/addtopic" method="post" enctype="multipart/form-data">
                     @csrf
                     <x-topics.topics-form topicsId="{{ $topicid }}"></x-topics.topics-form>
                 </form>
