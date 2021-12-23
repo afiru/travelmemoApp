e19dd755f8c92c758dc659371bde3ec95ac60560
Naoki Soga <n.soga@sunplacom>
2021-12-23 10:16:14 +0900

    修正

--- app/View/Components/topics/topicsArchives.php
+++ app/View/Components/topics/topicsArchives.php
@@ -20,13 +20,22 @@ class topicsArchives extends Component
 
 
 
-    public function __construct($limit = 0, $topics=null,$haslink=false,$haspaginate=false,$del=false)
+    public function __construct($limit = 0, $topics=null,$haslink=false,$haspaginate=false,$del=false,$topics=0)
     {
         $this->del = (boolean)$del;
         $this->limit = $limit;
         $this->haslink = $haslink;
         $this->haspaginate = $haspaginate;
-        $this->topics = topic::orderBy('UPDATED_AT','desc')->paginate(10);
+        $topics = topic::orderBy('UPDATED_AT','desc')->paginate(10);
+        if(empty($topics))
+        {
+            $this->topics = "";
+        }
+        else
+        {
+            $this->topics = $topics;
+        }
+
     }
 
     /**
